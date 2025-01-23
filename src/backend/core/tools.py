f46479bcc42e3f08ddcd9131a6474ee126e76e5b
Francis Secada <francis.secada@gmail.com>
2025-01-23 17:02:56 -0500

    enhancements: reddit intelligence gathering now ingests multiple subreddits, as defined by the comma-delimited `REDDIT_SUBREDDIT` env var; added `validate_result` validator to swot agent; frontend fixes

--- src/backend/core/tools.py
+++ src/backend/core/tools.py
@@ -1,12 +1,14 @@
+import asyncio
+
 import httpx
 from bs4 import BeautifulSoup as soup
-from pydantic_ai import RunContext
+from pydantic_ai import ModelRetry, RunContext
 
 from backend.core.consts import AI_MODEL
 from backend.core.core import SwotAgentDeps, SwotAnalysis, swot_agent
 from backend.core.utils import report_tool_usage
 from backend.logger import logger
-from backend.utils import get_val
+from backend.utils import get_val, set_event_loop, windows_sys_event_loop_check
 
 
 @swot_agent.tool(prepare=report_tool_usage)
@@ -76,7 +78,7 @@ async def analyze_competition(
 async def get_reddit_insights(
     ctx: RunContext[SwotAgentDeps],
     query: str,
-    subreddit_name: str | None = None,
+    subreddit_names: list[str] | None = None,
 ):
     """
     A tool to gain insights from a subreddit. Data is returned as string
@@ -84,23 +86,85 @@ async def get_reddit_insights(
 
     :param ctx: RunContext[SwotAgentDeps]
     :param query: str
-    :param subreddit_name: str
+    :param subreddit_names: str
     :return: str
     """
-    if not subreddit_name:
-        subreddit_name = get_val("REDDIT_SUBREDDIT", "python")
-    subreddit = ctx.deps.reddit_client.subreddit(subreddit_name)
-    search_results = subreddit.search(query)
-
+    if not subreddit_names:
+        subreddit_names = get_val("REDDIT_SUBREDDIT", "python, ")
+        subreddit_names = [x.strip() for x in subreddit_names.split(",")]
     insights = []
-    for post in search_results:
-        insights.append(
-            f"Title: {post.title}\n"
-            f"URL: {post.url}\n"
-            f"Content: {post.selftext}\n",
-        )
+    if len(subreddit_names) <= 3:
+        for name in subreddit_names:
+            subreddit = ctx.deps.reddit_client.subreddit(name)
+            search_results = subreddit.search(query)
+
+            for post in search_results:
+                insights.append(
+                    f"Title: {post.title}\n"
+                    f"URL: {post.url}\n"
+                    f"Content: {post.selftext}\n",
+                )
+    else:
+        windows_sys_event_loop_check()
+        set_event_loop()
+        loop = asyncio.get_event_loop()
+        tasks = [
+            asyncio.ensure_future(
+                loop.run_in_executor(
+                    None, ctx.deps.reddit_client.subreddit(name).search, query
+                )
+            )
+            for name in subreddit_names
+        ]
+        results = await asyncio.gather(*tasks)
+        for result in results:
+            for post in result:
+                insights.append(
+                    f"Title: {post.title}\n"
+                    f"URL: {post.url}\n"
+                    f"Content: {post.selftext}\n",
+                )
 
     return "\n".join(insights)
+
+
+@swot_agent.result_validator
+def validate_result(
+    _ctx: RunContext[SwotAgentDeps], value: SwotAnalysis
+) -> SwotAnalysis:
+    """
+    A validator for SWOT Analysis results; provides greater completeness and
+    quality control
+    :param _ctx: RunContext[SwotAgentDeps]
+    :param value: SwotAnalysis
+    :return: SwotAnalysis
+    """
+    issues = []
+    min = 2
+    categories = {
+        k.title(): getattr(value, k)
+        for k in ("strengths", "weaknesses", "opportunities", "threats")
+    }
+
+    for cat_name, points in categories.items():
+        if len(points) < min:
+            issues.append(
+                f"{cat_name} should have at least {min} points. "
+                f"Current count is {len(points)}."
+            )
+
+    min_len_analysis = 100
+    if len(value.analysis) < min_len_analysis:
+        issues.append(
+            f"Analysis should have at least {min_len_analysis} "
+            f"characters. Current count is {len(value.analysis)}."
+        )
+
+    if issues:
+        logger.info(f"Validation issues: {issues}")
+        raise ModelRetry("\n".join(issues))
+
+    return value
 
 
 async def run_agent(
